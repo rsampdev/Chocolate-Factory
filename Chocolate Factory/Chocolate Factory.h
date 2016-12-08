41226872ed57d7ec1639c5bceac50909e07e2d10
rsampdev <rsampdev@gmail.com>
2016-12-08 18:50:36 -0500

    Initial Commit

---
+++ Chocolate Factory/Chocolate Factory.h
@@ -0,0 +1,19 @@
+//
+//  Chocolate Factory.h
+//  Chocolate Factory
+//
+//  Created by Rodney Sampson on 12/8/16.
+//  Copyright © 2016 com.rsampdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chocolate Factory.
+FOUNDATION_EXPORT double Chocolate_FactoryVersionNumber;
+
+//! Project version string for Chocolate Factory.
+FOUNDATION_EXPORT const unsigned char Chocolate_FactoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chocolate_Factory/PublicHeader.h>
+
+
